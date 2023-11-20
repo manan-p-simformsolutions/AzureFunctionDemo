b44860d2b094743cdcb3fd374191d1725d30792a
Manan Patadiya <manan.p@simformsolutions.com>
2023-11-20 15:31:23 +0530

    Add project files.

---
+++ AzureFunctionDemo/EventFunction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionDemo
+{
+    public class EventFunction
+    {
+        [FunctionName("EventFunction")]
+        public void Run([BlobTrigger("mycontainer/{name}", Connection = "ConnectionString")] Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
